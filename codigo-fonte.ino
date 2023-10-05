70ffa228f700423c0a5d7ab2bff6d8e80f7f8cd6
juamori <juliaamorimbezerra@gmail.com>
2023-10-04 22:21:58 -0300

    atualizacao codigo fonte

--- codigo-fonte.ino
+++ codigo-fonte.ino
@@ -1,41 +1,40 @@
 #include "Arduino.h"
 #include "WiFi.h"
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-#include <LiquidCrystal_I2C.h>
-
-
-#define DHTPIN 7  
-#define DHTTYPE DHT11 
-
-// Portas
-const byte RED = 10;
-const byte BLUE = 11;
-const byte GREEN = 12;
-const int BUZZER = 8;
-int analogInPin = A0;
-int PinSensor = 6;
-#include "Arduino.h"
-#include "WiFi.h"
+#include "DHT.h"
 #include <HTTPClient.h>
+
+ 
+
+DHT dht(15, DHT11);
+
+ 
+
 HTTPClient client;
-char nomeWifi[] = "samsung fli3";
-char senhaWifi[] = "jubsleu19";
+char nomeWifi[] = "iPhone Airton";
+char senhaWifi[] = "2153818aa";
 char serverAddress[] = "https://api.tago.io/data";
 char contentHeader[] = "application/json";
-char tokenHeader[]   = "87ae2cb3-f941-45b6-8d8c-1e61f3ccd949";
-float temperatura = 0;
+char tokenHeader[] = "4400d025-0122-4d35-bde9-f2f621db6844";
+float umidade = 0; // Change the variable name to umidade (humidity)
+
+ 
+
 void setup() {
   Serial.begin(9600);
+  dht.begin();
   init_wifi();
 }
+
+ 
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  temperatura++;
-  send_temperatura();
+  umidade = dht.readHumidity(); // Read humidity instead of temperature
+  send_umidade(); // Call the function to send humidity data
   delay(5000);
 }
+
+ 
+
 void init_wifi() {
   Serial.println("Conectando WiFi");
   Serial.print("Wifi: ");
@@ -48,17 +47,20 @@ void init_wifi() {
   Serial.print("Meu IP eh: ");
   Serial.println(WiFi.localIP());
 }
-void send_temperatura() {
+
+ 
+
+void send_umidade() {
   char anyData[30];
   char postData[300];
   char anyData1[30];
   char bAny[30];
   int statusCode = 0;
-  strcpy(postData, "{\n\t\"variable\": \"temperature\",\n\t\"value\": ");
-  dtostrf(temperatura, 6, 2, anyData);
+  strcpy(postData, "{\n\t\"variable\": \"humidity\",\n\t\"value\": "); // Change variable to "humidity"
+  dtostrf(umidade, 6, 2, anyData);
   strncat(postData, anyData, 100);
-  strcpy(anyData1, ",\n\t\"unit\": \"C\"\n\t}\n");
-  strncat (postData, anyData1, 100);
+  strcpy(anyData1, ",\n\t\"unit\": \"%\"\n\t}\n"); // Change unit to "%"
+  strncat(postData, anyData1, 100);
   Serial.println(postData);
   client.begin(serverAddress);
   client.addHeader("Content-Type", contentHeader);
@@ -70,4 +72,27 @@ void send_temperatura() {
   Serial.println(statusCode);
   Serial.println("End of POST to TagoIO");
   Serial.println();
-} 
+
+ 
+
+  strcpy(postData, "{\n\t\"variable\": \"temperature\",\n\t\"value\": "); // Change variable to "humidity"
+  dtostrf(umidade, 6, 2, anyData);
+  strncat(postData, anyData, 100);
+  strcpy(anyData1, ",\n\t\"unit\": \"C\"\n\t}\n");
+  strncat(postData, anyData1, 100);
+  Serial.println(postData);
+  client.begin(serverAddress);
+  client.addHeader("Content-Type", contentHeader);
+  client.addHeader("Device-Token", tokenHeader);
+  statusCode = client.POST(postData);
+  delay(2000);
+  // read the status code and body of the response
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.println("End of POST to TagoIO");
+  Serial.println();
+
+ 
+
+  
+}
